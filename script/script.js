56e85a1b999ba6030fb22c05c38292fac3ec3d47
Md Shawon <shawonmohammad787@gmail.com>
2026-03-04 00:22:00 +0600

    lear vocabulary section

--- script/script.js
+++ script/script.js
@@ -1,1 +1,47 @@
-alert("hi")
+const loadLessons = () =>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLessons(json.data))
+};
+const loadLevelWord = (id) =>{
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data))
+}
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+    // wordContainer.innerHTML = "";
+    words.forEach(word =>{
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML=`
+        <p>Cat</p>
+        `;
+        wordContainer.append(card);
+    })
+}
+const displayLessons = (lessons) =>{
+    // 1-get the container & make empty
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML = "";
+    // 2.get into every lessons
+    for (let lesson of lessons){
+        // console.log(lesson);
+        
+        // 3. create element
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+        <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+            <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+          </button>
+        `
+    // 4. append child
+    levelContainer.append(btnDiv)
+    }
+    
+}
+
+loadLessons ();
